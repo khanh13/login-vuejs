ad6b89788eeea4aee9acd629a9426d8efeb869a9
Khanh <khanh371013@gmail.com>
2020-09-10 14:43:43 +0700

    Fixing loaded over HTTPS

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,15 +7,15 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta name="description" content="">
   <meta name="author" content="">
-  {{-- <base href="{{ asset('backend') }}"> --}}
+  <base href="">
 
-  <link href="{{ asset('backend/img/logo/logo.png')}}" rel="icon">
+  <link href="backend/img/logo/logo.png" rel="icon">
   <title>Login vue</title>
- <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css')}}">
-  <link href="{{ asset('backend/vendor/fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css">
-  <link href="{{ asset('backend/vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet" type="text/css">
-  <link href="{{ asset('backend/css/ruang-admin.min.css')}}" rel="stylesheet">
-  <link href="{{ asset('backend/vendor/datatables/dataTables.bootstrap4.min.css') }}" rel="stylesheet">
+ <link rel="stylesheet" type="text/css" href="css/app.css">
+  <link href="backend/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href="backend/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+  <link href="backend/css/ruang-admin.min.css" rel="stylesheet">
+  <link href="backend/vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
 </head>
 
 <body id="page-top">
@@ -30,9 +30,9 @@
     <i class="fas fa-angle-up"></i>
   </a>
 
-  <script src="{{ asset('js/app.js')}}"></script>
-  <script src="{{ asset('backend/vendor/jquery/jquery.min.js')}}"></script>
-  <script src="{{ asset('backend/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+  <script src="js/app.js"></script>
+  <script src="backend/vendor/jquery/jquery.min.js"></script>
+  <script src="backend/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
    <script type="text/javascript">
      let token = localStorage.getItem('token');
@@ -43,16 +43,16 @@
      }
    </script>
 
-  <script src="{{ asset('backend/vendor/jquery-easing/jquery.easing.min.js')}}"></script>
-  <script src="{{ asset('backend/js/ruang-admin.min.js')}}"></script>
-  <script src="{{ asset('backend/vendor/chart.js/Chart.min.js')}}"></script>
-  <script src="{{ asset('backend/js/demo/chart-area-demo.js')}}"></script>
+  <script src="backend/vendor/jquery-easing/jquery.easing.min.js"></script>
+  <script src="backend/js/ruang-admin.min.js"></script>
+  <script src="backend/vendor/chart.js/Chart.min.js"></script>
+  <script src="backend/js/demo/chart-area-demo.js"></script>
 
-  <script src="{{ asset('backend/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-  <script src="{{ asset('backend/vendor/jquery-easing/jquery.easing.min.js') }}"></script>
+  <script src="backend/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="backend/vendor/jquery-easing/jquery.easing.min.js"></script>
   <!-- Page level plugins -->
-  <script src="{{ asset('backend/vendor/datatables/jquery.dataTables.min.js') }}"></script>
-  <script src="{{ asset('backend/vendor/datatables/dataTables.bootstrap4.min.js') }}"></script>
+  <script src="backend/vendor/datatables/jquery.dataTables.min.js"></script>
+  <script src="backend/vendor/datatables/dataTables.bootstrap4.min.js"></script>
   <script>
     $(document).ready(function () {
       $('#dataTable').DataTable(); // ID From dataTable
