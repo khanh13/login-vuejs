821b36ac2bf41f8f983ca03d6dc89cd18b482075
Khanh <khanh371013@gmail.com>
2020-09-10 16:24:17 +0700

    fixing https v4

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,6 +7,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta name="description" content="">
   <meta name="author" content="">
+  <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"> 
   {{-- <base href="{{ asset('backend') }}"> --}}
 
   <link href="{{ asset('backend/img/logo/logo.png')}}" rel="icon">
